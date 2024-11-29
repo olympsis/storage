65da8feb5bd150dba2d9d47d93dce5118e7ece6a
josephjoeljo <joeljordanjoseph@gmail.com>
2024-11-29 13:37:07 -0500

    fix: cleaning up header

--- service/service.go
+++ service/service.go
@@ -46,7 +46,7 @@ func (s *Service) CORSHandler() http.HandlerFunc {
 		rw.Header().Set("Access-Control-Allow-Origin", "*")
 		rw.Header().Set("Access-Control-Allow-Credentials", "true")
 		rw.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		rw.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-Filename header")
+		rw.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-Filename")
 		rw.WriteHeader(http.StatusNoContent)
 	}
 }
