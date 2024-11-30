f37f53f1f060e42e33875689b10edcc1c6824b23
josephjoeljo <joeljordanjoseph@gmail.com>
2024-11-29 22:09:17 -0500

    fix: updated origin for uploads

--- middleware/cors.go
+++ middleware/cors.go
@@ -7,7 +7,8 @@ type Middleware func(http.HandlerFunc) http.HandlerFunc
 func CORS() Middleware {
 	return func(f http.HandlerFunc) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", "*")
+			origin := r.Header.Get("Origin")
+			w.Header().Set("Access-Control-Allow-Origin", origin)
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-Filename")
