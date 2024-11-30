e60c8d00b070af3d50cd1fe96a168bf62e7b833e
josephjoeljo <joeljordanjoseph@gmail.com>
2024-11-29 20:22:15 -0500

    chore: updaing the cors to help fix some issues?

--- middleware/cors.go
+++ middleware/cors.go
@@ -7,18 +7,18 @@ type Middleware func(http.HandlerFunc) http.HandlerFunc
 func CORS() Middleware {
 	return func(f http.HandlerFunc) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json")
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-Filename")
 
 			if r.Method == http.MethodOptions {
-				w.WriteHeader(http.StatusNoContent)
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
 				return
 			}
 
-			// call next middleware/handler in chain
+			w.Header().Set("Content-Type", "application/json")
 			f(w, r)
 		}
 	}
