c4b4abc60ca1591a4ce6a612236bd431ee72f433
josephjoeljo <joeljordanjoseph@gmail.com>
2025-02-21 22:28:08 -0500

    tweaks

--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,26 @@
 package utils
+
+import "os"
+
+func GetServerConfig() *ServerConfig {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+
+	logLevel := os.Getenv("LOG_LEVEL")
+	if logLevel == "" {
+		logLevel = "INFO"
+	}
+
+	firebaseFilePath := os.Getenv("FIREBASE_FILE_PATH")
+	if firebaseFilePath == "" {
+		panic("firebase file path required in environment variables")
+	}
+
+	return &ServerConfig{
+		Port:             port,
+		LogLevel:         logLevel,
+		FirebaseFilePath: firebaseFilePath,
+	}
+}
